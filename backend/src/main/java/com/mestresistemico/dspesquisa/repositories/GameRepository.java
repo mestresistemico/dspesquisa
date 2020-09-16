5f73554be57c23a212092f43cb419853adb9f82d
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-09-16 16:56:58 -0300

    DTO, Service

--- backend/src/main/java/com/mestresistemico/dspesquisa/repositories/GameRepository.java
+++ backend/src/main/java/com/mestresistemico/dspesquisa/repositories/GameRepository.java
@@ -1,9 +1,11 @@
 package com.mestresistemico.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mestresistemico.dspesquisa.entities.Game;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
 }
--- backend/src/main/java/com/mestresistemico/dspesquisa/repositories/GenreRepository.java
+++ backend/src/main/java/com/mestresistemico/dspesquisa/repositories/GenreRepository.java
@@ -1,9 +1,11 @@
 package com.mestresistemico.dspesquisa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mestresistemico.dspesquisa.entities.Genre;
 
+@Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
 }
